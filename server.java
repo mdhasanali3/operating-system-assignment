693126dc0395a4514f56d7c8eab7617f081c5ce2
mdhasanali3 <43574156+mdhasanali3@users.noreply.github.com>
2019-08-27 14:09:50 +0600

    Add files via upload

--- server.java
+++ server.java
@@ -1,33 +1,27 @@
-package socket;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+ package socket;
+
+import java.io.*;
+import java.net.*;
 
 public class server {
 
-	public static void main(String[] args) {
-		  try {System.out.println("Waiting for client ..");
-          ServerSocket ss=new ServerSocket(9806);
-          Socket soc=ss.accept();
-          System.out.println("connection established  ");
-          BufferedReader input=new BufferedReader(new InputStreamReader(soc.getInputStream()));
-      String str=input.readLine();
-      PrintWriter out=new PrintWriter(soc.getOutputStream(),true);
-      //out.flush();
-      out.print("server says "+str+"\n");
-     
-     
-     
-      }
-      catch (IOException e) {
-          // TODO Auto-generated catch block
-          e.printStackTrace();
-      }
-
-	}
-
+   
+    public static void main(String[] args) {
+            try {System.out.println("Waiting for client ..");
+            ServerSocket ss=new ServerSocket(9806);
+            Socket soc=ss.accept();
+            System.out.println("connection established  ");
+            
+            BufferedReader input=new BufferedReader(new InputStreamReader(soc.getInputStream()));
+        String str=input.readLine();
+        PrintWriter out=new PrintWriter(soc.getOutputStream(),true);
+        out.println("client saying to server  "+str);
+       System.out.print(in.readLine());
+       
+       ss.close();
+        }
+    catch(IOException e){e.printStackTrace();}
+    }
+    
 }
